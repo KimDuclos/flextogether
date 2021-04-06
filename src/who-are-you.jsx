b8a462a8377af9ffac812b0eb08fe51855464a03
Kim Duclos <kimduclos@gmail.com>
2021-04-06 13:15:09 -0700

    who are you responsive to iphonex

--- src/who-are-you.jsx
+++ src/who-are-you.jsx
@@ -6,37 +6,41 @@ import WhoAreYouButton from "./components/who-are-you-button.jsx";
 
 const WhoAreYou = () => {
   return (
-    <div className="who-are-you-page" >
+    <div className="who-are-you-page">
       <h1 className="who-are-you-title">Who are You?</h1>
       <p className="who-are-you-instructions">
         Please click your choice. Then click NEXT.
       </p>
       <div className="who-are-you-button-section">
-        <WhoAreYouButton
-          className="who-are-you-btn"
-          id="who-btn-1"
-          text="I am a senior looking for a fitness companion."
-        ></WhoAreYouButton>
-        <WhoAreYouButton
-          className="who-are-you-btn"
-          id="who-btn-2"
-          text="I would like to be a fitness companion for a senior"
-        ></WhoAreYouButton>
+        <div className="who-are-you-btn">
+          <WhoAreYouButton
+            id="who-btn-1"
+            text="I am a senior looking for a fitness companion."
+          ></WhoAreYouButton>
+        </div>
+        <div className="who-are-you-btn">
+          <WhoAreYouButton
+            id="who-btn-2"
+            text="I would like to be a fitness companion for a senior"
+          ></WhoAreYouButton>
+        </div>
       </div>
       <div className="who-are-you-prog-button-section">
         <Link to="/welcome-page" style={{ textDecoration: "none" }}>
-          <ProgressButton
-            className="prog-button"
-            id="who-are-you-back-btn"
-            text="BACK"
-          ></ProgressButton>
+          <div className="prog-button">
+            <ProgressButton
+              id="who-are-you-back-btn"
+              text="BACK"
+            ></ProgressButton>
+          </div>
         </Link>
         <Link to="/tell-more" style={{ textDecoration: "none" }}>
-          <ProgressButton
-            className="prog-button"
-            id="who-are-you-next-button"
-            text="NEXT"
-          ></ProgressButton>
+          <div className="prog-button">
+            <ProgressButton
+              id="who-are-you-next-button"
+              text="NEXT"
+            ></ProgressButton>
+          </div>
         </Link>
       </div>
     </div>
