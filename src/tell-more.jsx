a9cd159b18518bdd3a68ac64cf7ef9256ff4f98c
Kim Duclos <kimduclos@gmail.com>
2021-04-28 17:15:02 -0700

    radio buttons built, some alignment done

--- src/tell-more.jsx
+++ src/tell-more.jsx
@@ -26,65 +26,128 @@ const TellMore = () => {
   return (
     <div className="tell-more-page">
       <div className="tell-more-title">Tell us a bit more...</div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="firstName">First Name</label>
-        <input
-          name="firstName"
-          type="text"
-          ref={register({
-            required: "First name is required.",
-            pattern: {
-              value: /^[A-Za-z]+$/,
-              message: "First name can only have alphabetical characters.",
-            },
-          })}
-        />
-        {errors.firstName && errors.firstName.message && <p>{errors.firstName.message}</p>}
+      <form onSubmit={handleSubmit(onSubmit)} className="contact-form">
+        <div className="text-boxes">
+          <div className="box">
+            <div className="label">
+              <label htmlFor="firstName">First Name</label>
+            </div>
+            <div className="input">
+              <input
+                className="contact-input"
+                name="firstName"
+                type="text"
+                placeholder="First"
+                ref={register({
+                  required: "First name is required.",
+                  pattern: {
+                    value: /^[A-Za-z]+$/,
+                    message:
+                      "First name can only have alphabetical characters.",
+                  },
+                })}
+              />
+              {errors.firstName && errors.firstName.message && (
+                <p>{errors.firstName.message}</p>
+              )}
+            </div>
+          </div>
 
-        <label htmlFor="lastName">Last Name</label>
-        <input
-        name="lastName"
-        type="text"
-        ref={register({
-          required: "Last name is required.",
-          pattern: {
-            value: /^[A-Za-z]+$/,
-            message: "Last name can only have alphabetical characters."
-          },
-        })}  
-        />
-        {errors.lastName && errors.lastName.message && <p>{errors.lastName.message}</p>}
+          <div className="text-boxes">
+            <div className="box">
+              <div className="label">
+                <label htmlFor="lastName">Last Name</label>
+              </div>
+              <input
+                className="contact-input"
+                name="lastName"
+                type="text"
+                placeholder="Last"
+                ref={register({
+                  required: "Last name is required.",
+                  pattern: {
+                    value: /^[A-Za-z]+$/,
+                    message: "Last name can only have alphabetical characters.",
+                  },
+                })}
+              />
+              {errors.lastName && errors.lastName.message && (
+                <p>{errors.lastName.message}</p>
+              )}
+            </div>
+          </div>
 
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="email"
-          ref={register({
-            required: "Email is required.",
-            pattern: {
-              value: /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/,
-              message: "Email is not in correct format."
-            }
-          })}
-        />
-        {errors.email && errors.email.message && <p>{errors.email.message}</p>}
+          <div className="text-boxes">
+            <div className="box">
+              <div className="label">
+                <label htmlFor="email">Email</label>
+              </div>
+              <input
+                className="contact-input"
+                name="email"
+                // type="email"
+                placeholder="email@email.com"
+                ref={register({
+                  required: "Email is required.",
+                  pattern: {
+                    value: /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/,
+                    message: "Email is not in correct format.",
+                  },
+                })}
+              />
+              {errors.email && errors.email.message && (
+                <p>{errors.email.message}</p>
+              )}
+            </div>
+          </div>
 
-        <label htmlFor="phone">Phone</label>
-        <input
-          name="phone"
-          type="tel"
-          ref={register({
-            required: "Phone number is required.",
-            pattern: {
-              value: /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
-              message: "Phone number is not in correct format."
-            }
-          })}
-
-        />
-        {errors.phone && errors.phone.message && <p>{errors.phone.message}</p>}
-
-        <input type="submit" />
+          <div className="text-boxes">
+            <div className="box">
+              <div className="label">
+                <label htmlFor="phone">Phone</label>
+              </div>
+              <input
+                className="contact-input"
+                name="phone"
+                type="tel"
+                placeholder="555-555-5555"
+                ref={register({
+                  required: "Phone number is required.",
+                  pattern: {
+                    value: /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
+                    message: "Phone number is not in correct format.",
+                  },
+                })}
+              />
+              {errors.phone && errors.phone.message && (
+                <p>{errors.phone.message}</p>
+              )}
+            </div>
+          </div>
+        </div>
+        <label className="notification-title">Please click your prefered method of contact.</label>
+        <div className="notification-buttons">
+          <label>
+            <input
+              type="radio"
+              value="email"
+              name="notificationMethod"
+              value="Email"
+              checked
+            />{" "}
+            Email
+          </label>
+          <label>
+            <input type="radio" value="phone" name="notificationMethod" /> Phone
+          </label>
+          <label>
+            <input type="radio" value="both" name="notificationMethod" /> Both
+          </label>
+        </div>
+        <div className="tell-more-prog-btns">
+          <input type="submit" value="BACK" className="tell-more-prog-button" />
+          <input type="submit" value="NEXT" className="tell-more-prog-button" />
+        </div>
       </form>
     </div>
   );
